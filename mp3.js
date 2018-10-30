80572a03b16e82238f15181b33b021c5152ea609
ADMIN <admin@ADMINs-MacBook-Pro.local>
2018-10-30 15:30:32 +0700

    emkhonglamdc

---
+++ mp3.js
@@ -0,0 +1,37 @@
+var playlist1={
+    id :'1540304588828' ,
+    name : 'TÌnh Ca' ,
+    description : 'Tình Ca' ,
+    singger : 'Trọng Hiếu' ,
+    author: 'Quốc Bảo' ,
+    thumbnail:
+};
+var playlist2={
+    id :'1540304588828' ,
+    name : 'TÌnh Ca' ,
+    description : 'Tình Ca' ,
+    singger : 'Trọng Hiếu' ,
+    author: "Quốc Bảo" ,
+};
+var playlist3={
+    id :'1540304588828' ,
+    name : "TÌnh Ca" ,
+    description : 'Tình Ca' ,
+    singger : 'Trọng Hiếu' ,
+    author: 'Quốc Bảo'
+};
+var playlist4={
+    id :'1540304588828' ,
+    name : 'TÌnh Ca' ,
+    description : 'Tình Ca' ,
+    singger : 'Trọng Hiếu' ,
+    author: 'Quốc Bảo' ,
+};
+var playlist=new Array();
+playlist.push(playlist1);
+playlist.push(playlist2);
+playlist.push(playlist3);
+playlist.push(playlist4);
+for (var i = 0; i < playlist.length; i++) {
+
+}
